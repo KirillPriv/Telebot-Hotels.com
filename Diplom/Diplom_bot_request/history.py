dd4c8db9e1066f3afc5c8b33632374d603d59440
KirillPrivezencev <kirill.sochi@mail.ru>
2021-11-22 10:48:23 +0300

    В файл history.py добавлен доработанный код

--- Diplom/Diplom_bot_request/history.py
+++ Diplom/Diplom_bot_request/history.py
@@ -1,12 +1,10 @@
 import telebot
 
 
-def get_history(current_bot: telebot, message: telebot.types.Message) -> None:
+def get_history(bot: telebot, message: telebot.types.Message) -> None:
 
     """Основная функция которая выолняет вывод истории запросов пользователя
     из файла history {chat_id}.txt в чат бота"""
-
-    bot = current_bot
 
     try:
         with open('history {chat_id}.txt'.format(chat_id=message.chat.id), 'r', encoding='utf-8') as history:
