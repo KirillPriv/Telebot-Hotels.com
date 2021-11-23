e53e1666248e7636d8af16eeb1eee5395fc88431
KirillPrivezencev <kirill.sochi@mail.ru>
2021-11-23 20:53:45 +0300

    В файл history.py добавлен доработанный код

--- src/bot_requests/history.py
+++ src/bot_requests/history.py
@@ -1,6 +1,6 @@
 import telebot
 import datetime
-
+from loguru import logger
 
 def get_history(bot: telebot, message: telebot.types.Message) -> None:
 
@@ -12,8 +12,8 @@ def get_history(bot: telebot, message: telebot.types.Message) -> None:
             bot.send_message(message.chat.id, 'История запросов пользователя:\n{history}'.
                              format(history=history.read()))
     except FileNotFoundError as ex:
-        with open('errors id{chat_id}.log'.format(chat_id=message.chat.id), 'a', encoding='utf-8') as erros_log:
-            erros_log.write('\nИмя функции: {func}\n'.format(func=get_history.__name__))
-            erros_log.write('Вызвана ошибка: {traceback}'.format(traceback=ex))
-            erros_log.write('Время ошибки: {time}\n'.format(time=datetime.datetime.now().replace(microsecond=0)))
+        logger.add('errors id{chat_id}.log'.format(chat_id=message.chat.id), level='DEBUG',
+                   format='{time} {level} {message}', rotation='9:00', compression='zip')
+        logger.debug('Command: {command} Name func: {func} error name: {ex}'.
+                     format(command='history', func=get_history.__name__, ex=ex))
         bot.send_message(message.chat.id, 'История запросов пока пуста. Попробуйте позже')
