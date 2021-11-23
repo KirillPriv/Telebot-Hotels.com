a1411d53fa0724130a7ab939698d4fd7939124bf
KirillPrivezencev <kirill.sochi@mail.ru>
2021-11-23 15:54:18 +0300

    В файл history.py добавлен доработанный код

--- src/bot_requests/history.py
+++ src/bot_requests/history.py
@@ -1,4 +1,5 @@
 import telebot
+import datetime
 
 
 def get_history(bot: telebot, message: telebot.types.Message) -> None:
@@ -10,5 +11,9 @@ def get_history(bot: telebot, message: telebot.types.Message) -> None:
         with open('history {chat_id}.txt'.format(chat_id=message.chat.id), 'r', encoding='utf-8') as history:
             bot.send_message(message.chat.id, 'История запросов пользователя:\n{history}'.
                              format(history=history.read()))
-    except FileNotFoundError:
+    except FileNotFoundError as ex:
+        with open('errors id{chat_id}.log'.format(chat_id=message.chat.id), 'a', encoding='utf-8') as erros_log:
+            erros_log.write('\nИмя функции: {func}\n'.format(func=get_history.__name__))
+            erros_log.write('Вызвана ошибка: {traceback}'.format(traceback=ex))
+            erros_log.write('Время ошибки: {time}\n'.format(time=datetime.datetime.now().replace(microsecond=0)))
         bot.send_message(message.chat.id, 'История запросов пока пуста. Попробуйте позже')
