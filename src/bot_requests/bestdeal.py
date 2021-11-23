0089a3f80069be8823d2fd28d18e0acddf8c6d80
KirillPrivezencev <kirill.sochi@mail.ru>
2021-11-23 12:26:21 +0300

    В файл bestdeal.py добавлен доработанный код

--- src/bot_requests/bestdeal.py
+++ src/bot_requests/bestdeal.py
@@ -16,23 +16,23 @@ KEY_GET_HOTELS_INFO = os.getenv('KEY_GET_HOTELS_INFO')
 KEY_GET_HOTELS_FOTO = os.getenv('KEY_GET_HOTELS_FOTO')
 
 
-def start_search(bot: telebot, message: telebot.types.Message, User_dict: Dict) -> None:
+def start_search(bot: telebot, message: telebot.types.Message, user_dict: Dict) -> None:
     """Функция старта команды. Запрашивает у пользователя город, в котором расположен отель"""
 
     bot.send_message(message.from_user.id, 'В каком городе будем осуществлять поиск по команде /bestdeal\n'
                                            'Пример ввода русских городов: Moscow Russia или moscow Russia\n'
                                            'Пример ввода иностранных городов: London или london')
-    bot.register_next_step_handler(message, get_city, bot, User_dict)
+    bot.register_next_step_handler(message, get_city, bot, user_dict)
 
 
-def get_city(message: telebot.types.Message, bot: telebot, User_dict: Dict) -> None:
+def get_city(message: telebot.types.Message, bot: telebot, user_dict: Dict) -> None:
     """Функция, котороя по названию города делает запрос на API и записывает
     полученный результат destinationId в User_dict"""
 
     try:
         if message.text.isalpha() or len(message.text.split()) == 2 and message.text.split()[0].isalpha() \
                 and message.text.split()[1].isalpha():
-            User_dict[message.chat.id]['city'] = message.text
+            user_dict[message.chat.id]['city'] = message.text
             url = 'https://hotels4.p.rapidapi.com/locations/v2/search'
             querystring = {'query': message.text,
                            'locale': 'en_US',
@@ -51,71 +51,71 @@ def get_city(message: telebot.types.Message, bot: telebot, User_dict: Dict) -> N
                 hotel_destinationId = dict_hotels_id['suggestions'][0]['entities'][0]['destinationId']
                 User_dict[message.chat.id]['hotel_destinationId'] = hotel_destinationId
 
-                chekIn_hotel(message, bot, User_dict)
+                chekIn_hotel(message, bot, user_dict)
             else:
                 raise
         else:
             raise
     except:
         bot.send_message(message.from_user.id, 'Город введен неверно, введите город согласно интрукции')
-        bot.register_next_step_handler(message, get_city, bot, User_dict)
+        bot.register_next_step_handler(message, get_city, bot, user_dict)
 
 
-def chekIn_hotel(message: telebot.types.Message, bot: telebot, User_dict: Dict) -> None:
+def chekIn_hotel(message: telebot.types.Message, bot: telebot, user_dict: Dict) -> None:
     """Функция, котороя запрашивает у пользователя дату заезда в отель"""
 
     bot.send_message(message.from_user.id, 'Введите дату заезда в отель через (-)\n'
                                            'Пример ввода даты: 20-11-2021')
-    bot.register_next_step_handler(message, chekOut_hotel, bot, User_dict)
+    bot.register_next_step_handler(message, chekOut_hotel, bot, user_dict)
 
 
-def chekOut_hotel(message: telebot.types.Message, bot: telebot, User_dict: Dict) -> None:
+def chekOut_hotel(message: telebot.types.Message, bot: telebot, user_dict: Dict) -> None:
     """Функция, котороя запрашивает у пользователя дату выезда из отеля"""
 
     try:
         if len(message.text.split('-')) == 3 and 0 < int(message.text.split('-')[0]) <= 31 \
                 and 0 < int(message.text.split('-')[1]) <= 12 and 0 < int(message.text.split('-')[2]) >= 2021:
 
-            User_dict[message.chat.id]['chekIn'] = message.text
+            user_dict[message.chat.id]['chekIn'] = message.text
             bot.send_message(message.from_user.id, 'Введите дату выезда из отеля через (-)\n'
                                                    'Пример ввода даты: 28-11-2021')
-            bot.register_next_step_handler(message, period_of_stay_hotel, bot, User_dict)
+            bot.register_next_step_handler(message, period_of_stay_hotel, bot, user_dict)
         else:
             raise
     except:
         bot.send_message(message.from_user.id, 'Дата введена некоректно, введите дату согласно интрукции')
-        bot.register_next_step_handler(message, chekOut_hotel, bot, User_dict)
+        bot.register_next_step_handler(message, chekOut_hotel, bot, user_dict)
 
 
-def period_of_stay_hotel(message: telebot.types.Message, bot: telebot, User_dict: Dict) -> None:
+def period_of_stay_hotel(message: telebot.types.Message, bot: telebot, user_dict: Dict) -> None:
     """Функция, котороя подсчитывает количесвто дней, которые пользователь проведет в отеле"""
 
     try:
         if len(message.text.split('-')) == 3 and 0 < int(message.text.split('-')[0]) <= 31 \
                 and 0 < int(message.text.split('-')[1]) <= 12 and 0 < int(message.text.split('-')[2]) >= 2021:
 
-            User_dict[message.chat.id]['chekOut'] = message.text
+            user_dict[message.chat.id]['chekOut'] = message.text
 
-            date_chek_in = User_dict[message.chat.id]['chekIn'].split('-')
-            date_chek_out = User_dict[message.chat.id]['chekOut'].split('-')
+            date_chek_in = user_dict[message.chat.id]['chekIn'].split('-')
+            date_chek_out = user_dict[message.chat.id]['chekOut'].split('-')
             period_of_stay = date(int(date_chek_out[2]), int(date_chek_out[1]), int(date_chek_out[0])) - \
                              date(int(date_chek_in[2]), int(date_chek_in[1]), int(date_chek_in[0]))
 
-            User_dict[message.chat.id]['period_of_stay'] = period_of_stay.days
-            get_hotel_info(message, bot, User_dict)
+            user_dict[message.chat.id]['period_of_stay'] = period_of_stay.days
+            get_hotel_info(message, bot, user_dict)
         else:
             raise
     except:
         bot.send_message(message.from_user.id, 'Дата введена некоректно, введите дату согласно интрукции')
-        bot.register_next_step_handler(message, period_of_stay_hotel, bot, User_dict)
+        bot.register_next_step_handler(message, period_of_stay_hotel, bot, user_dict)
 
 
-def get_hotel_info(message: telebot.types.Message, bot: telebot, User_dict: Dict) -> None:
+def get_hotel_info(message: telebot.types.Message, bot: telebot, user_dict: Dict) -> None:
     """Функция, котороя по destinationId запрашивает на API инфомрацию по отелям
     и передает полученный результат в виде словаря hotels_dict в функцию get_number_city()"""
 
     url = 'https://hotels4.p.rapidapi.com/properties/list'
-    querystring = {'destinationId': User_dict[message.chat.id]['hotel_destinationId'],
+    querystring = {'destinationId': user_dict[message.chat.id]['hotel_destinationId'],
                    'pageNumber': '1',
                    'pageSize': '25',
                    'checkIn:': time.strftime('%Y-%m-%d'),
@@ -135,66 +135,66 @@ def get_hotel_info(message: telebot.types.Message, bot: telebot, User_dict: Dict
     hotels = json.loads(req_hotels.text)
     hotels_dict = hotels['data']['body']['searchResults']['results']
 
-    get_range_price(message, hotels_dict, bot, User_dict)
+    get_range_price(message, hotels_dict, bot, user_dict)
 
 
-def get_range_price(message: telebot.types.Message, hotels_dict: Dict, bot: telebot, User_dict: Dict) -> None:
+def get_range_price(message: telebot.types.Message, hotels_dict: Dict, bot: telebot, user_dict: Dict) -> None:
     """Функция, котороя запрашивает у пользователя диапазон стоимости проживания,
             по которому необходимо осуществлять поиск отелей"""
 
-    User_dict[message.chat.id]['hotels'] = hotels_dict
+    user_dict[message.chat.id]['hotels'] = hotels_dict
     bot.send_message(message.from_user.id, 'Введите диапазон стоимости проживания через (-)\n'
                                            'Пример ввода диапазона стоимости: 3-40')
-    bot.register_next_step_handler(message, get_range_distance, bot, User_dict)
+    bot.register_next_step_handler(message, get_range_distance, bot, user_dict)
 
 
-def get_range_distance(message: telebot.types.Message, bot: telebot, User_dict: Dict) -> None:
+def get_range_distance(message: telebot.types.Message, bot: telebot, user_dict: Dict) -> None:
     """Функция, котороя запрашивает у пользователя диапазон расстояния от центра города,
         по которому необходимо осуществлять поиск отелей"""
 
     try:
         if len(message.text.split('-')) == 2 and int(message.text.split('-')[0]) <= int(message.text.split('-')[1]):
-            User_dict[message.chat.id]['range_price'] = message.text
+            user_dict[message.chat.id]['range_price'] = message.text
             bot.send_message(message.from_user.id, 'Введите диапазон расстояния от центра города через (-)\n'
                                                    'Пример ввода расстояния от центра: 0-3')
-            bot.register_next_step_handler(message, get_number_city, bot, User_dict)
+            bot.register_next_step_handler(message, get_number_city, bot, user_dict)
         else:
             raise
     except:
         bot.send_message(message.from_user.id, 'Введенно некорректное значение, '
                                                'либо первое значение привышает второе веденное значение\n'
                                                'Введите значение согласно интрукции')
-        bot.register_next_step_handler(message, get_range_distance, bot, User_dict)
+        bot.register_next_step_handler(message, get_range_distance, bot, user_dict)
 
 
-def get_number_city(message: telebot.types.Message, bot: telebot, User_dict: Dict) -> None:
+def get_number_city(message: telebot.types.Message, bot: telebot, user_dict: Dict) -> None:
     """Функция, котороя запрашивает у пользователя количество отелей,
     которое необходимо вывести в чат"""
 
     try:
         if len(message.text.split('-')) == 2 and int(message.text.split('-')[0]) <= int(message.text.split('-')[1]):
-            User_dict[message.chat.id]['range_distance'] = message.text
+            user_dict[message.chat.id]['range_distance'] = message.text
             bot.send_message(message.from_user.id, 'Сколько вывести отелей с заданными параметрами в городе {city}\n'
                                                    'Примечание: Количество отелей не должно быть больше 25'.
-                             format(city=User_dict[message.chat.id]['city']))
-            bot.register_next_step_handler(message, get_foto, bot, User_dict)
+                             format(city=user_dict[message.chat.id]['city']))
+            bot.register_next_step_handler(message, get_foto, bot, user_dict)
         else:
             raise
     except:
         bot.send_message(message.from_user.id, 'Введенно некорректное значение, '
                                                'либо первое значение привышает второе веденное значение\n'
                                                'Введите значение согласно интрукции')
-        bot.register_next_step_handler(message, get_number_city, bot, User_dict)
+        bot.register_next_step_handler(message, get_number_city, bot, user_dict)
 
 
-def get_foto(message: telebot.types.Message, bot: telebot, User_dict: Dict) -> None:
+def get_foto(message: telebot.types.Message, bot: telebot, user_dict: Dict) -> None:
     """Функция, котороя запрашивает у пользователя нужно ли выводить фотографии отелей"""
 
     try:
         if message.text.isalnum() and int(message.text) <= 25:
-            User_dict[message.chat.id]['hotels_number'] = message.text
+            user_dict[message.chat.id]['hotels_number'] = message.text
             with open('User_dict {chat_id}.json'.format(chat_id=message.chat.id), 'w') as file:
-                json.dump(User_dict, file, indent=4)
+                json.dump(user_dict, file, indent=4)
             keyboard = types.InlineKeyboardMarkup()
             keyboard.add(types.InlineKeyboardButton(text='Да', callback_data='yes'))
             keyboard.add(types.InlineKeyboardButton(text='Нет', callback_data='no'))
@@ -206,27 +206,27 @@ def get_foto(message: telebot.types.Message, bot: telebot, User_dict: Dict) -> N
         bot.send_message(message.from_user.id, 'Введенно некорректное значение, '
                                                'либо введенное значение привышает 25\n'
                                                'Введите значение согласно интрукции (кол-во <= 25)')
-        bot.register_next_step_handler(message, get_foto, bot, User_dict)
+        bot.register_next_step_handler(message, get_foto, bot, user_dict)
 
 
-def get_quantity_foto(message: telebot.types.Message, bot: telebot, User_dict: Dict) -> None:
+def get_quantity_foto(message: telebot.types.Message, bot: telebot, user_dict: Dict) -> None:
     """Функция, котороя запрашивает у пользователя количество фотографий отелей,
     которое необходимо вывести в чат"""
 
     bot.send_message(message.chat.id, 'Сколько фото отелей вывести\n'
                                       'Примечание: Количество фото не должно быть больше 10')
-    bot.register_next_step_handler(message, get_city_price_and_foto, bot, User_dict)
+    bot.register_next_step_handler(message, get_city_price_and_foto, bot, user_dict)
 
 
-def get_city_price_and_foto(message: telebot.types.Message, bot: telebot, User_dict: Dict) -> None:
+def get_city_price_and_foto(message: telebot.types.Message, bot: telebot, user_dict: Dict) -> None:
     """Основная Функция для вывода информации по выбранным отелям и фотографий к ним,
     в данной функции производится сортировка словаря по выбранным параметрам, а
     также осуществляется вывод информации по отелям в чат"""
 
-    User_dict[message.chat.id]['get_foto'] = ['yes']
-    hotels_dict = User_dict[message.chat.id]['hotels']
-    range_price = User_dict[message.chat.id]['range_price'].split('-')
-    range_distance = User_dict[message.chat.id]['range_distance'].split('-')
+    user_dict[message.chat.id]['get_foto'] = ['yes']
+    hotels_dict = user_dict[message.chat.id]['hotels']
+    range_price = user_dict[message.chat.id]['range_price'].split('-')
+    range_distance = user_dict[message.chat.id]['range_distance'].split('-')
 
     try:
         if message.text.isalnum() and int(message.text) <= 10:
@@ -240,14 +240,14 @@ def get_city_price_and_foto(message: telebot.types.Message, bot: telebot, User_d
                     return False
 
             hotel_dict_filter = list(filter(filter_hotel, hotels_dict))
-            if len(hotel_dict_filter) < int(User_dict[message.chat.id]['hotels_number']):
+            if len(hotel_dict_filter) < int(user_dict[message.chat.id]['hotels_number']):
                 bot.send_message(message.chat.id, 'К сожалению по вашим параметрам, отелей нашлось '
                                                   'всего: {range}\n'.format(range=len(hotel_dict_filter)))
 
             hotel_dict_sorted = sorted(hotel_dict_filter, key=lambda elem: elem['ratePlan']['price']['exactCurrent'],
                                        reverse=False)
 
-            for i_hotel in hotel_dict_sorted[:int(User_dict[message.chat.id]['hotels_number'])]:
+            for i_hotel in hotel_dict_sorted[:int(user_dict[message.chat.id]['hotels_number'])]:
                 url = 'https://hotels4.p.rapidapi.com/properties/get-hotel-photos'
                 querystring = {'id': i_hotel['id'],  # конкретный id отеля
                                }
@@ -259,7 +259,7 @@ def get_city_price_and_foto(message: telebot.types.Message, bot: telebot, User_d
                 req_hotels_foto = requests.request('GET', url, headers=headers, params=querystring)
                 hotels_foto_dict = json.loads(req_hotels_foto.text)
 
-                total_price = float(User_dict[message.chat.id]['period_of_stay']) * \
+                total_price = float(user_dict[message.chat.id]['period_of_stay']) * \
                               round(i_hotel['ratePlan']['price']['exactCurrent'], 0)
 
                 bot.send_message(message.chat.id, 'Наименование отеля: \n{name}\n'
@@ -286,18 +286,18 @@ def get_city_price_and_foto(message: telebot.types.Message, bot: telebot, User_d
         bot.send_message(message.from_user.id, 'Введенно некорректное значение, '
                                                'либо введенное значение привышает 10\n'
                                                'Введите значение согласно интрукции (кол-во фото <= 10)')
-        bot.register_next_step_handler(message, get_city_price_and_foto, bot, User_dict)
+        bot.register_next_step_handler(message, get_city_price_and_foto, bot, user_dict)
 
 
-def get_city_price_none_foto(message: telebot.types.Message, bot: telebot, User_dict: Dict) -> None:
+def get_city_price_none_foto(message: telebot.types.Message, bot: telebot, user_dict: Dict) -> None:
     """Основная Функция для вывода информации по выбранным отелям без фотографий,
        в данной функции производится сортировка словаря по выбранным параметрам, а
        также осуществляется вывод информации по отелям в чат"""
 
-    User_dict[message.chat.id]['get_foto'] = ['yes']
-    hotels_dict = User_dict[message.chat.id]['hotels']
-    range_price = User_dict[message.chat.id]['range_price'].split('-')
-    range_distance = User_dict[message.chat.id]['range_distance'].split('-')
+    user_dict[message.chat.id]['get_foto'] = ['yes']
+    hotels_dict = user_dict[message.chat.id]['hotels']
+    range_price = user_dict[message.chat.id]['range_price'].split('-')
+    range_distance = user_dict[message.chat.id]['range_distance'].split('-')
 
     def filter_hotel(hotel_dict):
         if 'ratePlan' in hotel_dict:
@@ -309,15 +309,15 @@ def get_city_price_none_foto(message: telebot.types.Message, bot: telebot, User_
             return False
 
     hotel_dict_filter = list(filter(filter_hotel, hotels_dict))
-    if len(hotel_dict_filter) < int(User_dict[message.chat.id]['hotels_number']):
+    if len(hotel_dict_filter) < int(user_dict[message.chat.id]['hotels_number']):
         bot.send_message(message.chat.id, 'К сожалению по вашим параметрам, отелей нашлось '
                                           'всего: {range}\n'.format(range=len(hotel_dict_filter)))
 
     hotel_dict_sorted = sorted(hotel_dict_filter, key=lambda elem: elem['ratePlan']['price']['exactCurrent'],
                                reverse=False)
 
-    for i_hotel in hotel_dict_sorted[:int(User_dict[message.chat.id]['hotels_number'])]:
-        total_price = float(User_dict[message.chat.id]['period_of_stay']) * \
+    for i_hotel in hotel_dict_sorted[:int(user_dict[message.chat.id]['hotels_number'])]:
+        total_price = float(user_dict[message.chat.id]['period_of_stay']) * \
                       round(i_hotel['ratePlan']['price']['exactCurrent'], 0)
 
         bot.send_message(message.chat.id, 'Наименование отеля: \n{name}\n'
